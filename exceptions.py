c5f9956b29f1f5c3e7efcbf259b27b5316f63526
Annie Kramer <anniekramer@dropbox.com>
2014-07-08 00:39:22 -0700

    Add except block and error message

--- exceptions.py
+++ exceptions.py
@@ -1,12 +1,18 @@
+import os.path
+import sys
+
 while True: 
     try:
         filename = raw_input("Please enter a valid filename. \n< ")
         count = 0
         with open(filename, 'r') as f:
             for line in f:
-            count += 1
-        print count
-
+                count += 1
+        print "There are",count, "lines in your file."
+        break
+    except IOError:
+        print "Apologies! There was an error with your file."
+        sys.exit()
 
 
 
